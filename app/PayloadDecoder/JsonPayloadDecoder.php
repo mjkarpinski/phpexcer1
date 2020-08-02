d2e54525447a62d96b9b43ac05f1168cf7d54cfd
karp <karp@karp.space>
2020-08-02 15:30:38 +0100

    Payload has only one entry - remove iteration

--- app/PayloadDecoder/JsonPayloadDecoder.php
+++ app/PayloadDecoder/JsonPayloadDecoder.php
@@ -7,41 +7,41 @@ use App\DTO\Campaign;
 use App\DTO\Payload;
 use App\DTO\QueryType;
 
+use Exception;
+
 final class JsonPayloadDecoder implements PayloadDecoderInterface
 {
     public function decode(string $payload)
     {
-        $decodedPayload = json_decode($payload);
+        $payloadEntry = json_decode($payload);
 
-        $payloadEntries = [];
-        foreach ($decodedPayload as $payloadEntry) {
-            if (!$this->validateEntry($payloadEntry)) {
-                //Log message about broken data
-                continue;
-            }
-
-            $queryType = new QueryType($payloadEntry['query_type']['id'], $payloadEntry['query_type']['title']);
-            $callStats = new CallStats(
-                $payloadEntry['call_stats']['id'],
-                $payloadEntry['call_stats']['length'],
-                $payloadEntry['call_stats']['recording_url']
-            );
-
-            $campaign = new Campaign(
-                $payloadEntry['campaign']['id'],
-                $payloadEntry['campaign']['name'],
-                $payloadEntry['campaign']['description']
-            );
-
-            $payloadEntries[] = new Payload(
-                $payloadEntry['name'],
-                $payloadEntry['phone'],
-                $payloadEntry['email'],
-                $queryType,
-                $callStats,
-                $campaign
-            );
+        if (!$this->validateEntry($payloadEntry)) {
+            //Log message about broken data
+            throw new Exception();
         }
+
+        $queryType = new QueryType($payloadEntry['query_type']['id'], $payloadEntry['query_type']['title']);
+        $callStats = new CallStats(
+            $payloadEntry['call_stats']['id'],
+            $payloadEntry['call_stats']['length'],
+            $payloadEntry['call_stats']['recording_url']
+        );
+
+        $campaign = new Campaign(
+            $payloadEntry['campaign']['id'],
+            $payloadEntry['campaign']['name'],
+            $payloadEntry['campaign']['description']
+        );
+
+        $payloadEntries[] = new Payload(
+            $payloadEntry['name'],
+            $payloadEntry['phone'],
+            $payloadEntry['email'],
+            $queryType,
+            $callStats,
+            $campaign
+        );
+
 
         return $payloadEntries;
     }
