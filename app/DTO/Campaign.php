5fc3e7c64c67df1a616d887c20834c580631256a
karp <karp@karp.space>
2020-08-02 14:47:23 +0100

    Add Campaign DTO

---
+++ app/DTO/Campaign.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\DTO;
+
+final class Campaign
+{
+    private $id;
+    private $name;
+    private $description;
+
+    public function __construct(int $id, string $name, string $description)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->description = $description;
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+}
