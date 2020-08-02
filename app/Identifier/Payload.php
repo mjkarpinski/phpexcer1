93f68229daa64f0e2a7c11aea56b86aeccab286c
karp <karp@karp.space>
2020-08-02 15:09:23 +0100

    Add Payload identifier

---
+++ app/Identifier/Payload.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Identifier;
+
+final class Payload
+{
+    private const JSON_TYPE = 'json';
+
+    public static function identifyType(string $payload)
+    {
+        if (is_object(json_decode($payload)) || is_array(json_decode($payload))) {
+            return self::JSON_TYPE;
+        }
+    }
+}
